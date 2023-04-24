a9e90183b762b67add12d6695774b39d5ac3bdf4
柚子 <chowhound_xb@163.com>
2023-04-24 13:54:55 +0000

    多线程搜索
    Signed-off-by: 柚子 <chowhound_xb@163.com>

--- src/apis/api.js
+++ src/apis/api.js
@@ -4,22 +4,26 @@ import { AARCH64_SEARCH_IP, X86_SEARCH_IP, appListUrls } from './https'
 export const searchApps = async (keyword, arch = '') => {
   try {
     const url = arch === 'aarch64' ? AARCH64_SEARCH_IP : X86_SEARCH_IP;
-    const allData = [];
-    for (const [key, value] of Object.entries(appListUrls)) {
-      const response = await axios.get(url+value);
-      const data = response.data;
-      const filteredData = data.filter((item) => item.Name.toLowerCase().includes(keyword.toLowerCase()));
-      const typedData = filteredData.map((item) => ({
-        ...item,
-        type: key,
-        jsonurl: `${url}${key}/${item.Pkgname}/app.json`,
-        iconurl: `${url}${key}/${item.Pkgname}/icon.png`
+    const promises = Object.entries(appListUrls).map(([key, value]) => axios.get(url+value)
+      .then((response) => {
+        const data = response.data;
+        const filteredData = data.filter((item) =>
+          item.Name.toLowerCase().includes(keyword.toLowerCase()));
+        return filteredData.map((item) => ({
+          ...item,
+          type: key,
+          jsonurl: `${url}${key}/${item.Pkgname}/app.json`,
+          iconurl: `${url}${key}/${item.Pkgname}/icon.png`
       }));
-      allData.push(...typedData);
-    }
-    console.log(allData);
-    return allData;
-  } catch (error) {
-    console.error(error);
-  }
+    })
+    .catch((error) => {
+      console.error(error);
+      return [];
+    })
+  );
+  const allData = await Promise.all(promises).then((result) => result.flat());
+  return allData;
+} catch (error) {
+  console.error(error);
+}
 };
