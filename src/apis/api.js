821936054ec1f8a0216caac04abf35d940b2cefc
柚子 <chowhound_xb@163.com>
2023-04-24 15:29:39 +0000

    更新搜索页面
    Signed-off-by: 柚子 <chowhound_xb@163.com>

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 import { AARCH64_SEARCH_IP, X86_SEARCH_IP, appListUrls } from './https'
 
+export const typeList=[]
+
 export const searchApps = async (keyword, arch = '') => {
   try {
+    typeList.length = 0;
+    typeList.push("all");
     const url = arch === 'aarch64' ? AARCH64_SEARCH_IP : X86_SEARCH_IP;
     const promises = Object.entries(appListUrls).map(([key, value]) => axios.get(url+value)
       .then((response) => {
         const data = response.data;
         const filteredData = data.filter((item) =>
           item.Name.toLowerCase().includes(keyword.toLowerCase()));
+        
+          // 如果有符合条件的结果，将当前请求的类型添加到typeList中
+          if (filteredData.length > 0) {
+            typeList.push(key);
+          }
+
         return filteredData.map((item) => ({
           ...item,
           type: key,
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
-    <!-- <br />
-    <br /> -->
+    <div class="category-selector">
+      <button v-for="(value, key) in categories"
+              :class="{active: activeCategory === key}"
+              @click="selectCategory(key)"
+              v-if="includesTypeList(key)">
+        {{value}}
+      </button>
+    </div>
+
     <center>
-      <div class="list" v-if="isShow">
-        <!--        单个应用模块-->
-        <PkgItem
-          v-for="item in list"
-          :key="item.id"
-          :item="{Name: item.Name, More: item.More}"
-          :img-src="ReplaceUrl(item.iconurl)"
-          @click="GotoJson(ReplaceUrl(item.jsonurl))" />
+      <div class="search-list" v-if="isShow">
+        <!-- 单个应用模块-->
+        <PkgItem v-for="item in filteredList" :key="item.id"
+                 :item="{Name: item.Name, More: item.More}"
+                 :img-src="ReplaceUrl(item.iconurl)"
+                 @click="GotoJson(ReplaceUrl(item.jsonurl))" />
       </div>
     </center>
+
     <center>
       <div class="notice" v-if="isEmpty">
         <h1 class="title">暂时没有找到相关应用 :)</h1>
       </div>
     </center>
   </div>
-
 </template>
 
 <script>
-import { searchApps } from '@/apis/api'
+import { searchApps, typeList } from '@/apis/api'
 import PkgItem from "../components/PkgItem.vue";
 
 export default {
@@ -35,19 +40,40 @@ export default {
       isShow: false,//判断是否有搜索，没有不创建app列表
       isEmpty: false,
       searchInput: "",
-      downloadContent: "DOWNLOAD"
+      downloadContent: "DOWNLOAD",
+      categories: {
+        all: "全部",
+        chat: "聊天",
+        development: "开发",
+        games: "游戏",
+        image_graphics: "图像与图形",
+        music: "音乐",
+        network: "网络",
+        office: "办公",
+        others: "其他",
+        reading: "阅读",
+        themes: "主题",
+        tools: "工具",
+        video: "视频",
+      },
+      activeCategory: 'all', // 默认选中 All 分类
     };
+  },
+  computed: {
+    filteredList() {
+      // 根据当前选中的分类筛选出符合条件的列表
+      if (this.activeCategory === 'all') {
+        return this.list;
+      } else {
+        return this.list.filter(item => item.type === this.activeCategory);
+      }
+    },
   },
   methods: {
     GotoJson(icon) {
       let appUrl = icon.replace('icon.png', 'app.json')
       console.log(appUrl);
-      /* console.log(
-        `https://d.store.deepinos.org.cn//store/${this.category}/${pkgn}/app.json`
-        //app.json 软件的详细信息
-      ); */
       window.open(
-        //pkgn 在仓库中的包名
         appUrl,
         "_self",
         ""
@@ -57,38 +83,57 @@ export default {
       return icon.replace(/\+/g,'%2B')
     },
     async Search() {
-      console.log(this.$route.query.keywords);
       if (this.$route.query.keywords) {
         const res = await searchApps(this.$route.query.keywords,this.$route.query.arch);
+        
         this.list = res;
-        this.result = this.list.length;
-        console.log(this.result)
+        this.isShow = true;
         if (this.result === 0) {
           this.isEmpty = true;
-          this.isShow = false;
         } else {
           this.isEmpty = false;
-          this.isShow = true;
         }
       }
+    },
+    selectCategory(category) {
+      // 更新当前选中的分类
+      this.activeCategory = category;
+    },
+    includesTypeList(key){
+      return typeList.includes(key);
     }
   },
   async mounted() {
     this.Search();
+    // 获取category-selector元素
+    const categorySelector = document.querySelector('.category-selector');
+
+    // 监听页面滚动事件
+    window.addEventListener('scroll', function() {
+      // 获取滚动条的scrollTop属性
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+      if (scrollTop > 60) {
+        // scrollTop大于0时，添加阴影样式
+        categorySelector.classList.add('shadow');
+      } else {
+        // 否则移除阴影样式
+        categorySelector.classList.remove('shadow');
+      }
+    });
   },
-  // 监听,当路由发生变化的时候执行
+  beforeDestroy() {
+    // 移除页面滚动事件的监听器
+    window.removeEventListener('scroll', function() {});
+  },
   watch: {
     $route: {
       handler: function (val, oldVal) {
         console.log(val);
         this.Search();
       },
-      // 深度观察监听
-      deep: true
+      deep: true,
     },
   },
-
-
-
 };
 </script>
