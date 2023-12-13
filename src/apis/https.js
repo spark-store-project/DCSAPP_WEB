8b15c1dd4638fd0ca9bb833b39c1109dc72c7c5e
shenmo <jifengshenmo@outlook.com>
2023-12-13 11:40:54 +0000

    木兰平台加速
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- src/apis/https.js
+++ src/apis/https.js
@@ -17,6 +17,6 @@ export const appListUrls = {
 export const AARCH64_SEARCH_IP = window.location.origin.includes('localhost') || window.location.origin.includes('127.0.0.1') ? 'https://d.store.deepinos.org.cn/aarch64-store' : `${window.location.origin}/aarch64-store`;
 export const X86_SEARCH_IP = window.location.origin.includes('localhost') || window.location.origin.includes('127.0.0.1') ? 'https://d.store.deepinos.org.cn/store' : `${window.location.origin}/store`;
 
-export const IMG_AARCH64_SEARCH_IP = 'https://img-d.store.deepinos.org.cn/aarch64-store';
-export const IMG_X86_SEARCH_IP = 'https://img-d.store.deepinos.org.cn/store';
+export const IMG_AARCH64_SEARCH_IP = 'http://120.132.17.134:3000/better-dde/png-accelerate/raw/branch/master/aarch64-store';
+export const IMG_X86_SEARCH_IP = 'http://120.132.17.134:3000/better-dde/png-accelerate/raw/branch/master/store/';
 
